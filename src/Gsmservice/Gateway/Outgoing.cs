1f7500cf2bd053397c6ef591348f393f8b1b98a5
tommekk83 <tomasz@gsmservice.pl>
2024-10-13 09:28:03 +0200

    Upgrade SDK version to 0.0.14
    Updated the SDK version from 0.0.13 to 0.0.14 across various files and altered the `User-Agent` string accordingly. Additionally, modified the GitHub actions workflow to allow manual dispatch and improved the NuGet package push process.

--- src/Gsmservice/Gateway/Outgoing.cs
+++ src/Gsmservice/Gateway/Outgoing.cs
@@ -79,10 +79,10 @@ namespace Gsmservice.Gateway
     {
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
-        private const string _sdkVersion = "0.0.13";
+        private const string _sdkVersion = "0.0.14";
         private const string _sdkGenVersion = "2.438.3";
         private const string _openapiDocVersion = "0.9.2";
-        private const string _userAgent = "speakeasy-sdk/csharp 0.0.13 2.438.3 0.9.2 Gsmservice.Gateway";
+        private const string _userAgent = "speakeasy-sdk/csharp 0.0.14 2.438.3 0.9.2 Gsmservice.Gateway";
         private string _serverUrl = "";
         private ISpeakeasyHttpClient _client;
         private Func<Gsmservice.Gateway.Models.Components.Security>? _securitySource;
